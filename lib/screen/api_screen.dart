0009fa3873dfe4f1559f0291d7a13b37c778ee60
morenorpl <morenoibrahim606@gmail.com>
2025-09-04 09:57:45 +0700

    refactor: changed padding layout api.screen

--- lib/screen/api_screen.dart
+++ lib/screen/api_screen.dart
@@ -138,25 +138,28 @@ class _ApiScreenState extends State<ApiScreen> {
                     ),
                   ),
                 ),
-                SizedBox(height: 12),
-
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    spacing: 10,
-                    children: [
-                      categoryButton('Semua', ''),
-                      categoryButton('National', 'nasional'),
-                      categoryButton('International', 'internasional'),
-                      categoryButton('Ekonomi', 'ekonomi'),
-                      categoryButton('Olahraga', 'olahraga'),
-                      categoryButton('Teknologi', 'teknologi'),
-                      categoryButton('hiburan', 'hiburan'),
-                      categoryButton('Gaya-hidup', 'gaya-hidup'),
-                    ],
-                  ),
-                ),
               ],
+            ),
+          ),
+          SizedBox(height: 12),
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                spacing: 10,
+                children: [
+                  categoryButton('Semua', ''),
+                  categoryButton('National', 'nasional'),
+                  categoryButton('International', 'internasional'),
+                  categoryButton('Ekonomi', 'ekonomi'),
+                  categoryButton('Olahraga', 'olahraga'),
+                  categoryButton('Teknologi', 'teknologi'),
+                  categoryButton('hiburan', 'hiburan'),
+                  categoryButton('Gaya-hidup', 'gaya-hidup'),
+                ],
+              ),
             ),
           ),
           SizedBox(height: 12),
@@ -179,74 +182,80 @@ class _ApiScreenState extends State<ApiScreen> {
                             ),
                           );
                         },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 12,
-                            vertical: 12,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color(0x2020200D),
-                                spreadRadius: 3,
-                                blurRadius: 6,
-                                offset: Offset(0, 3),
-                              ),
-                            ],
-                          ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 12,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0x2020200D),
+                                  spreadRadius: 3,
+                                  blurRadius: 6,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                            ),
 
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                height: 120,
-                                width: 110,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(10),
-                                  image: DecorationImage(
-                                    image: NetworkImage(item['image']['small']),
-                                    fit: BoxFit.cover,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 120,
+                                  width: 110,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        item['image']['small'],
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(width: 20),
-                              SizedBox(
-                                width: 220,
-                                height: 120,
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Text(
-                                        item['title'],
-                                        maxLines: 3,
+                                SizedBox(width: 20),
+                                SizedBox(
+                                  width: 220,
+                                  height: 120,
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          item['title'],
+                                          maxLines: 3,
 
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w700,
-                                          height: 1.2,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w700,
+                                            height: 1.2,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Text(
-                                      formatDate(item['isoDate']),
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500,
+                                      Text(
+                                        formatDate(item['isoDate']),
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500,
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       );
